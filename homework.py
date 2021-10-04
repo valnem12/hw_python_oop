c36f32f1b8e63c72932d4864a66767862d83f0a1
Valentin Nemanov <kat-ne@yandex.ru>
2021-10-03 20:54:36 -0400

    version 2

--- homework.py
+++ homework.py
@@ -1,3 +1,13 @@
+'''
+Thank you very much for your review.
+I highly appreciate the hard work you do.
+
+After your comments I feel I got a lighter
+and smarter code.
+
+Special thanks for the links you attached.
+'''
+
 
 import datetime as dt
 
@@ -5,23 +15,20 @@ DATE_FORMAT = '%d.%m.%Y'
 
 
 class Record():
-    '''Createsa list of a new uploaded data.'''
+    "Createsa list of a new uploaded data."
 
     def __init__(self, amount, comment=None, date=None) -> None:
         self.amount = amount
         self.comment = comment
         self.date = self.date2day(date)
-        # self.save()
 
     def date2day(self, day):
-        '''Converting str(date) into date format'''
+        "Converting str(date) into date format"
 
-        if type(day) == str:
+        if day:
             return dt.datetime.strptime(day, DATE_FORMAT).date()
-        # elif type(day) != str:
-        #     return day
-        else:
-            return dt.datetime.now().date()
+
+        return dt.datetime.now().date()
 
 
 class Calculator:
@@ -35,87 +42,54 @@ class Calculator:
     def __init__(self, limit) -> None:
         self.limit = limit
         self.records = []
-        self.now = dt.datetime.now().date()
 
     def add_record(self, record):
-        '''Adding record to calories burning history.'''
+        "Adding record to calories burning history."
 
         self.records.append(record)
         return self.records
 
-    def get_conversion_rate(self, currency):
-        '''Returns conversion rate based on the currency'''
-
-        if currency == 'eur':
-            conversion = self.EURO_RATE
-        elif currency == 'usd':
-            conversion = self.USD_RATE
-        elif currency == 'rub':
-            conversion = self.RUB_RATE
-        else:
-            return (
-                'Please check the currency input '
-                'Available options are: "rub", "usd" or "eur"')
-        return conversion
+    def today(self):
+        "Returns today's date"
+        self.now = dt.datetime.now().date()
+        return self.now
 
     def total_today_spendings(self):
-        '''Calculates daily cash spendings.'''
+        "Calculates daily spendings."
+        now = self.today()
+        return sum(spent.amount for spent
+                   in self.records
+                   if spent.date == now)
 
-        sum = 0
-        for i, spent in enumerate(self.records):
-            if self.records[i].date == self.now:
-                sum += self.records[i].amount
-        return sum
+    def get_week_stats(self):
+        "Calculates amount spent for last seven days."
 
-    def total_week_spendings(self):
-        '''Calculates weekly cash spendings.'''
+        week = self.today() - dt.timedelta(days=7)
+        return sum(spent.amount for spent in self.records
+                   if (spent.date >= week
+                       and spent.date <= self.today()))
 
-        sum = 0
-        for i, spent in enumerate(self.records):
-            if (self.records[i].date >= self.now - dt.timedelta(days=7)
-                    and self.records[i].date <= self.now):
-                sum += self.records[i].amount
-        return sum
+    def get_today_stats(self):
+        "Returns todays' spendings."
 
-    def get_week_stats(self, currency=None):
-        '''Calculates amount spent for last seven days.'''
+        return self.total_today_spendings()
 
-        if currency:
-            conversion_rate = self.get_conversion_rate(currency)
-
-            if isinstance(conversion_rate, str):
-                return conversion_rate
-
-            else:
-                return (
-                    'Weekly cash spendings are '
-                    f'{self.total_week_spendings(self.records):.2f}')
-
-        return self.total_week_spendings()
-
-    def get_today_stats(self, currency=None):
-        '''Returns todays' spendings.'''
-
-        if currency:
-            conversion_rate = self.get_conversion_rate(currency)
-            return self.total_today_spendings() / conversion_rate
-
-        else:
-            return self.total_today_spendings()
+    def remain(self):
+        return (self.limit - self.get_today_stats())
 
 
 class CaloriesCalculator(Calculator):
-    '''Calculates remained calories.'''
+    "Calculates remained calories."
 
     def __init__(self, limit) -> None:
         super().__init__(limit)
-        self.records = []
 
     def get_calories_remained(self):
-        remained = (self.limit
-                    - self.get_today_stats())
+        "Summing all blown calories and returns recommendation."
+
+        remained = super().remain()
         if remained > 0:
-            return (f'Сегодня можно съесть что-нибудь ещё,'
+            return ('Сегодня можно съесть что-нибудь ещё,'
                     f' но с общей калорийностью не более {remained} кКал')
         else:
             return 'Хватит есть!'
@@ -135,42 +109,30 @@ class CashCalculator(Calculator):
 
     def __init__(self, limit) -> None:
         super().__init__(limit)
-        self.records = []
-        self.currency_dict = {'usd': 'USD',
-                              'eur': 'Euro',
-                              'rub': 'руб'}
+        self.currency_dict = {'usd': (self.USD_RATE, 'USD'),
+                              'eur': (self.EURO_RATE, 'Euro'),
+                              'rub': (self.RUB_RATE, 'руб')}
 
     def add_record(self, record):
-        '''
-        I have no idea why it's here - code can run without it
-        but test fails
-        '''
+        "Adding new data to usage records"
 
         self.records = super().add_record(record)
         return self.records
 
     def get_today_cash_remained(self, currency):
-        '''Calculates remaining cash to spend today.'''
+        "Calculates remaining cash to spend today."
 
-        if isinstance(super().get_conversion_rate(currency), str):
-            return super().get_conversion_rate(currency)
+        if not currency:
+            raise ValueError('Available options are: "rub", "usd" or "eur"')
+
         else:
-            remained = ((self.limit
-                         - self.total_today_spendings())
-                        / super().get_conversion_rate(currency))
+            remained = (super().remain()
+                        / self.currency_dict[currency][0])
 
-            if remained > 0:
-                return (f'На сегодня осталось {remained:.2f} '
-                        f'{self.currency_dict[currency]}')
-            elif remained == 0:
+            if remained == 0:
                 return 'Денег нет, держись'
-            else:
-                return ('Денег нет, держись: твой долг - '
-                        f'{-1*remained:.2f} {self.currency_dict[currency]}')
-
-        return super().get_today_stats(currency)
-
-    def get_week_cash_remained(self, currency):
-        '''Calculates spendings within last seven days.'''
-
-        return super().get_week_stats(currency)
+            elif remained > 0:
+                return (f'На сегодня осталось {remained:.2f} '
+                        f'{self.currency_dict[currency][1]}')
+            return ('Денег нет, держись: твой долг - '
+                    f'{-1*remained:.2f} {self.currency_dict[currency][1]}')
