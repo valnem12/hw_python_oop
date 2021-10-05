69e1bd6ec1aa6d2d00208d9f74dff6457175fbcb
Valentin Nemanov <kat-ne@yandex.ru>
2021-10-05 06:56:03 -0400

    v.5

--- homework.py
+++ homework.py
@@ -4,7 +4,7 @@ DATE_FORMAT = '%d.%m.%Y'
 
 
 class Record():
-    "Creates a list of a new uploaded data."
+    """Creates a list of a new uploaded data."""
 
     def __init__(self, amount, comment=None, date=None) -> None:
         self.amount = amount
@@ -12,7 +12,7 @@ class Record():
         self.date = self.date2day(date)
 
     def date2day(self, day):
-        "Converting str(date) into date format"
+        """Converting str(date) into date format"""
 
         if day:
             return dt.datetime.strptime(day, DATE_FORMAT).date()
@@ -33,17 +33,17 @@ class Calculator:
         self.records = []
 
     def add_record(self, record):
-        "Adding record to calories burning history."
+        """Adding record to calories burning history."""
 
         self.records.append(record)
 
     def today(self):
-        "Returns today's date"
+        """Returns today's date"""
 
         return dt.date.today()
 
     def total_today_spendings(self):
-        "Calculates daily spendings."
+        """Calculates daily spendings."""
 
         now = self.today()
         return sum(spent.amount for spent
@@ -51,7 +51,7 @@ class Calculator:
                    if spent.date == now)
 
     def get_week_stats(self):
-        "Calculates amount spent for last seven days."
+        """Calculates amount spent for last seven days."""
 
         now = self.today()
         week = now - dt.timedelta(days=7)
@@ -60,7 +60,7 @@ class Calculator:
                        and spent.date <= now))
 
     def get_today_stats(self):
-        "Returns todays' spendings."
+        """Returns todays' spendings."""
 
         return self.total_today_spendings()
 
@@ -69,10 +69,10 @@ class Calculator:
 
 
 class CaloriesCalculator(Calculator):
-    "Calculates remained calories."
+    """Calculates remained calories."""
 
     def get_calories_remained(self):
-        "Summing all blown calories and returns recommendation."
+        """Summing all blown calories and returns recommendation."""
 
         remained = super().remain()
         if remained > 0:
@@ -89,9 +89,9 @@ class CashCalculator(Calculator):
     Requires currency parameter.
     """
 
-    USD_RATE = 60.
-    EURO_RATE = 70.
-    RUB_RATE = 1.
+    USD_RATE = 60.0
+    EURO_RATE = 70.0
+    RUB_RATE = 1.0
 
     def __init__(self, limit) -> None:
         super().__init__(limit)
@@ -100,22 +100,21 @@ class CashCalculator(Calculator):
                               'rub': (self.RUB_RATE, 'руб')}
 
     def get_today_cash_remained(self, currency):
-        "Calculates remaining cash to spend today."
+        """Calculates remaining cash to spend today."""
 
         currency_keys = ('", "').join(self.currency_dict)
 
         if currency not in self.currency_dict:
             raise ValueError(f'Available options are: "{currency_keys}"')
 
-        else:
-            remained = (super().remain()
-                        / self.currency_dict[currency][0])
+        remained = (super().remain()
+                    / self.currency_dict[currency][0])
 
-            if remained == 0:
-                return 'Денег нет, держись'
+        if remained == 0:
+            return 'Денег нет, держись'
 
-            if remained > 0:
-                return (f'На сегодня осталось {remained:.2f} '
-                        f'{self.currency_dict[currency][1]}')
-            return ('Денег нет, держись: твой долг - '
-                    f'{-1*remained:.2f} {self.currency_dict[currency][1]}')
+        if remained > 0:
+            return (f'На сегодня осталось {remained:.2f} '
+                    f'{self.currency_dict[currency][1]}')
+        return ('Денег нет, держись: твой долг - '
+                f'{-1*remained:.2f} {self.currency_dict[currency][1]}')
